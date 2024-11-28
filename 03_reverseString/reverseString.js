00a848a578e9ba4ecfbe06e099dcbb350795713a
dankdungeon <davidduonggg@gmail.com>
2024-11-27 18:37:07 -0800

    Exercise 3
    Finsihed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let res = '';
+  for (let i = string.length-1; i >= 0; i--) {
+    const letter = string[i]
+    res += letter;
+  }
+  return res;
 };
 
 // Do not edit below this line
